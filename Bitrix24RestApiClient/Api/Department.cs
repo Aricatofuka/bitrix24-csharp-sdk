ee726a9acaa4d11658fe3a68cccc61c7a8ba090e
a.grishin <a.grishin@trace-studio.com>
2024-07-16 18:48:51 +0300

    Код ревью

--- Bitrix24RestApiClient/Api/Department.cs
+++ Bitrix24RestApiClient/Api/Department.cs
@@ -1,6 +1,7 @@
 ﻿using BXRest.Core.Client;
 using BXRest.Core.Models.Response;
 using BXRest.Core.Models;
+using BXRest.Models.Tasks.Task;
 using System.Threading.Tasks;
 using BXRest.Models.Department;
 using System.Collections.Generic;
@@ -8,9 +9,16 @@ using System.Collections.Generic;
 namespace BXRest.Api
 {
     /// Набор api методов для подразделений
-    /// Конструктор
-    public class Department(IBitrix24Client client)
+    public class Department
     {
+        private IBitrix24Client client;
+
+        /// Конструктор
+        public Department(IBitrix24Client client)
+        {
+            this.client = client;
+        }
+
         /// Получение фильтрованного списка подразделений
         public async Task<iBXRespnse<List<iGetItem>>> Get(iGetParam param)
         {
